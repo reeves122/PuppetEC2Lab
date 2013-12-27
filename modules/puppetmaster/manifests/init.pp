7492294aaed5927cf3c58e9c22e58df54a84cab1
Justin Reeves <reeves122@gmail.com>
2013-12-27 16:56:42 +0000

    fixed cron

--- modules/puppetmaster/manifests/init.pp
+++ modules/puppetmaster/manifests/init.pp
@@ -1,7 +1,7 @@
 class puppetmaster {
 
   cron { git_pull :
-    command => "cd /etc/puppet/;git pull",
+    command => "cd /etc/puppet/;/usr/bin/git pull",
     user    => root,
     minute  => '*/1'
     }
