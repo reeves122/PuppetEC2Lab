e1f9f68150a7221b94ed246b8756c01b4a5021a8
Justin Reeves <reeves122@gmail.com>
2013-12-27 11:02:35 -0600

    added bash profile

--- modules/osbase/manifests/init.pp
+++ modules/osbase/manifests/init.pp
@@ -8,4 +8,12 @@ class osbase {
     mode    => '644',
   }
 
+  file { '/root/.bash_profile':
+    ensure  => present,
+    source  => 'puppet:///modules/osbase/bash_profile',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '644',
+  }
+
 }
